59e417d13821b6780c3655f0fd96b646df226c5e
Andrew Zhukov <oxyblade@gmail.com>
2019-10-29 23:01:28 +0300

    improvements

--- js/main.js
+++ js/main.js
@@ -1,21 +1,14 @@
-/* ========================================================================= */
-/*	Preloader
-/* ========================================================================= */
-
+/* Preloader */
 jQuery(window).load(function () {
 
   $("#preloader").fadeOut("slow");
 
 });
 
-/* ========================================================================= */
-/*  Welcome Section Slider
-/* ========================================================================= */
-
+/* Slider */
 $(function () {
 
   var Page = (function () {
-
     var $navArrows = $('#nav-arrows'),
         $nav = $('#nav-dots > span'),
         slitslider = $('#slider').slitslider({
@@ -23,56 +16,38 @@ $(function () {
 
             $nav.removeClass('nav-dot-current');
             $nav.eq(pos).addClass('nav-dot-current');
-
           }
         }),
 
         init = function () {
-
           initEvents();
-
         },
         initEvents = function () {
-
-          // add navigation events
           $navArrows.children(':last').on('click', function () {
-
             slitslider.next();
             return false;
-
           });
 
           $navArrows.children(':first').on('click', function () {
-
             slitslider.previous();
             return false;
 
           });
 
           $nav.each(function (i) {
-
             $(this).on('click', function (event) {
-
               var $dot = $(this);
-
               if (!slitslider.isActive()) {
-
                 $nav.removeClass('nav-dot-current');
                 $dot.addClass('nav-dot-current');
-
               }
 
               slitslider.jump(i + 1);
               return false;
-
             });
-
           });
-
         };
-
     return {init: init};
-
   })();
 
   Page.init();
@@ -82,10 +57,7 @@ $(function () {
 
 $(document).ready(function () {
 
-  /* ========================================================================= */
-  /*	Menu item highlighting
-  /* ========================================================================= */
-
+  /* Nav */
   jQuery('#nav').singlePageNav({
     offset: jQuery('#nav').outerHeight(),
     filter: ':not(.external)',
@@ -112,20 +84,15 @@ $(document).ready(function () {
     }
   });
 
-  /* ========================================================================= */
-  /*	Fix Slider Height
-  /* ========================================================================= */
-
   // Slider Height
   var slideHeight = $(window).height();
 
   $('#home-slider, #slider, .sl-slider, .sl-content-wrapper').css('height', slideHeight);
 
   $(window).resize(function () {
-    'use strict',
-        $('#home-slider, #slider, .sl-slider, .sl-content-wrapper').css('height', slideHeight);
+    'use strict';
+    $('#home-slider, #slider, .sl-slider, .sl-content-wrapper').css('height', slideHeight);
   });
-
 
   $("#works, #testimonial").owlCarousel({
     navigation: true,
@@ -137,10 +104,7 @@ $(document).ready(function () {
   });
 
 
-  /* ========================================================================= */
-  /*	Featured Project Lightbox
-  /* ========================================================================= */
-
+  /* Featured Project Lightbox */
   $(".fancybox").fancybox({
     padding: 0,
 
@@ -168,102 +132,9 @@ $(document).ready(function () {
       }
     }
   });
-
 });
 
-
-/* ==========  START GOOGLE MAP ========== */
-
-// When the window has finished loading create our google map below
-
-/*google.maps.event.addDomListener(window, 'load', init);
-
-function init() {
-    // Basic options for a simple Google Map
-    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-
-	    var myLatLng = new google.maps.LatLng(22.402789, 91.822156);
-
-	    var mapOptions = {
-	        zoom: 15,
-	        center: myLatLng,
-	        disableDefaultUI: true,
-	        scrollwheel: false,
-	        navigationControl: true,
-	        mapTypeControl: false,
-	        scaleControl: false,
-	        draggable: true,
-
-        // How you would like to style the map.
-        // This is where you would paste any style found on Snazzy Maps.
-        styles: [{
-            featureType: 'water',
-            stylers: [{
-                color: '#46bcec'
-            }, {
-                visibility: 'on'
-            }]
-        }, {
-            featureType: 'landscape',
-            stylers: [{
-                color: '#f2f2f2'
-            }]
-        }, {
-            featureType: 'road',
-            stylers: [{
-                saturation: -100
-            }, {
-                lightness: 45
-            }]
-        }, {
-            featureType: 'road.highway',
-            stylers: [{
-                visibility: 'simplified'
-            }]
-        }, {
-            featureType: 'road.arterial',
-            elementType: 'labels.icon',
-            stylers: [{
-                visibility: 'off'
-            }]
-        }, {
-            featureType: 'administrative',
-            elementType: 'labels.text.fill',
-            stylers: [{
-                color: '#444444'
-            }]
-        }, {
-            featureType: 'transit',
-            stylers: [{
-                visibility: 'off'
-            }]
-        }, {
-            featureType: 'poi',
-            stylers: [{
-                visibility: 'off'
-            }]
-        }]
-    };
-
-    // Get the HTML DOM element that will contain your map
-    // We are using a div with id="map" seen below in the <body>
-    var mapElement = document.getElementById('map-canvas');
-
-    // Create the Google Map using our element and options defined above
-    var map = new google.maps.Map(mapElement, mapOptions);
-
-    // Let's also add a marker while we're at it
-    var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(22.402789, 91.822156),
-        map: map,
-		icon: 'img/icons/map-marker.png',
-    });
-}*/
-
-// ========== END GOOGLE MAP ========== //
-
 // ========== TO THE TOP PAGE - HEADER ========== //
-
 $(window).scroll(function () {
   if ($(this).scrollTop() > 1) {
     $('.back-to-header').fadeIn('slow');
