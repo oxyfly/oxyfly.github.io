2b53785b24d141ccd8753c192ab96c48483ef531
andrew <oxyblade@gmail.com>
2020-03-17 16:36:29 +0300

    updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,98 +1,116 @@
+$(document).ready(function () {
+  $("#testimonial-slider").owlCarousel({
+    items: 2,
+    itemsDesktop: [1000, 2],
+    itemsDesktopSmall: [979, 2],
+    itemsTablet: [768, 1],
+    pagination: false,
+    navigation: true,
+    navigationText: ["", ""],
+    autoPlay: true
+  });
+});
 
-$(document).ready(function(){
-    $("#testimonial-slider").owlCarousel({
-        items:2,
-        itemsDesktop:[1000,2],
-        itemsDesktopSmall:[979,2],
-        itemsTablet:[768,1],
-        pagination:false,
-        navigation:true,
-        navigationText:["",""],
-        autoPlay:true
+$(document).ready(function () {
+  var w = $(window).width();
+
+  if (w < 400) {
+    $(".regular").slick({
+      lazyLoad: 'ondemand',
+      dots: true,
+      infinite: false,
+      slidesToShow: 3,
+      slidesToScroll: 3
     });
+  } else {
+    $(".regular").slick({
+      lazyLoad: 'ondemand',
+      dots: true,
+      infinite: false,
+      slidesToShow: 5,
+      slidesToScroll: 5,
+      responsive: [
+        {
+          breakpoint: 1200,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            arrows: false,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 400,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            arrows: false,
+            dots: true
+          }
+        },
+      ]
+    });
+  }
 });
 
-$( document ).ready(function() {
-   var w = $(window).width();
-
-   if(w < 400){
-      alert();
-       $(".regular").slick({
-         lazyLoad: 'ondemand',
-        dots: true,
-        infinite: false,
-        slidesToShow: 3,
-        slidesToScroll: 3
-      });
-   }else{
-        $(".regular").slick({
-         lazyLoad: 'ondemand',
-        dots: true,
-        infinite: false,
-        slidesToShow: 6,
-        slidesToScroll: 6
-      });
-   }
+$(document).ready(function () {
+  $('#nav-head').scrollToFixed();
 });
 
-$( document ).ready(function() {
-    $('#nav-head').scrollToFixed();
-})
 
+$(document).ready(function () {
 
-$( document ).ready(function() {
+  var revapi;
+  jQuery(document).ready(function () {
+    revapi = jQuery('.fullwidthbanner').revolution({
 
-                var revapi;
-                jQuery(document).ready(function() {
-          revapi = jQuery('.fullwidthbanner').revolution({
+      delay: 9000,
+      startwidth: 1140,
+      startheight: 500,
+      hideThumbs: 200,
 
-            delay:9000,
-            startwidth:1140,
-            startheight:500,
-            hideThumbs:200,
+      thumbWidth: 100,
+      thumbHeight: 50,
+      thumbAmount: 3,
 
-            thumbWidth:100,
-            thumbHeight:50,
-            thumbAmount:3,
+      navigationType: "none",
+      navigationArrows: "solo",
+      navigationStyle: "round",
 
-            navigationType:"none",
-            navigationArrows:"solo",
-            navigationStyle:"round",
+      touchenabled: "on",
+      onHoverStop: "on",
 
-            touchenabled:"on",
-            onHoverStop:"on",
+      navigationHAlign: "center",
+      navigationVAlign: "bottom",
+      navigationHOffset: 0,
+      navigationVOffset: 20,
 
-            navigationHAlign:"center",
-            navigationVAlign:"bottom",
-            navigationHOffset:0,
-            navigationVOffset:20,
+      soloArrowLeftHalign: "left",
+      soloArrowLeftValign: "center",
+      soloArrowLeftHOffset: 20,
+      soloArrowLeftVOffset: 0,
 
-            soloArrowLeftHalign:"left",
-            soloArrowLeftValign:"center",
-            soloArrowLeftHOffset:20,
-            soloArrowLeftVOffset:0,
+      soloArrowRightHalign: "right",
+      soloArrowRightValign: "center",
+      soloArrowRightHOffset: 20,
+      soloArrowRightVOffset: 0,
 
-            soloArrowRightHalign:"right",
-            soloArrowRightValign:"center",
-            soloArrowRightHOffset:20,
-            soloArrowRightVOffset:0,
+      shadow: 0,
+      fullWidth: "on",
+      fullScreen: "off",
+      lazyLoad: "on",
 
-            shadow:0,
-            fullWidth:"on",
-            fullScreen:"off",
-            lazyLoad:"on",
+      stopLoop: "on",
+      stopAfterLoops: -1,
+      stopAtSlide: -1,
 
-            stopLoop:"on",
-            stopAfterLoops:-1,
-            stopAtSlide:-1,
+      shuffle: "off",
 
-            shuffle:"off",
-
-            hideSliderAtLimit:0,
-            hideCaptionAtLimit:0,
-            hideAllCaptionAtLilmit:0,
-            startWithSlide:0,
-                  });
-});
+      hideSliderAtLimit: 0,
+      hideCaptionAtLimit: 0,
+      hideAllCaptionAtLilmit: 0,
+      startWithSlide: 0,
+    });
+  });
 
 });
